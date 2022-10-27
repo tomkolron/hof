f53027ecc85fa882e1409ec352e9a53791912cf0
mech-keyboard69420 <keyboard69420@protonmail.com>
2022-10-28 02:49:38 +0300

    fix comments format

--- src/headers.rs
+++ src/headers.rs
@@ -6,10 +6,10 @@ use indicatif::{ProgressBar,ProgressStyle};
 #[tokio::main]
 
 pub async fn get_headers(urls: Vec<String>) -> Result<HashMap<&'static str, String>, Box<dyn std::error::Error>>{
-    //Declare progress bar variable
+    // Declare progress bar variable
     let progress = ProgressBar::new(urls.len().try_into().unwrap());
 
-    //Style progress bar
+    // Style progress bar
     progress.set_style(
         ProgressStyle::with_template(
             "[{elapsed}] [{bar:60.green}] {pos}/{len}, ETA {eta}",
@@ -18,7 +18,7 @@ pub async fn get_headers(urls: Vec<String>) -> Result<HashMap<&'static str, Stri
         .progress_chars("=> ")
     );
 
-    //Crate empty vectors
+    // Crate empty vectors
     let mut headers: Vec<String> = Vec::new();
     
     let mut valid_urls: Vec<String> = Vec::new();
@@ -29,12 +29,12 @@ pub async fn get_headers(urls: Vec<String>) -> Result<HashMap<&'static str, Stri
      
     println!("Getting headers for all domains ...");
 
-    //Itirate through all the urls
+    // Itirate through all the urls
     for url in urls.iter() {
-        //Check if url is valid
+        // Check if url is valid
         match make_req(url.to_string()).await {
             Ok(res) => {
-                //Push title to headers vec
+                // Push title to headers vec
                 if url == &urls[0] {
                     headers.push(String::from(format!("Url: {}\n\n", url)));
                 }else {
@@ -56,31 +56,31 @@ pub async fn get_headers(urls: Vec<String>) -> Result<HashMap<&'static str, Stri
                 }
 
                 for i in 0..headers_keys.len() {
-                    //Push http header to headers vec
+                    // Push http header to headers vec
                     headers.push(format!("{}: {}\n", headers_keys[i], headers_values[i]));
                 }
             },
-            //If url is not valid
+            // If url is not valid
             Err(_error) => {
-                //Push url to false_urls vec
+                // Push url to false_urls vec
                 false_urls.push(String::from(format!("{}\n", url)));
             },
         }
-        //Increment the progress bar
+        // Increment the progress bar
         progress.inc(1);
     }
-    //Finish progress bar
+    // Finish progress bar
     progress.finish();
     println!("");
 
-    //Return header, valid_url, false_urls
+    // Return header, valid_url, false_urls
     hashmap.insert("headers", headers.join(""));
     hashmap.insert("valid_urls", valid_urls.join(""));
     hashmap.insert("false_urls", false_urls.join(""));
     return Ok(hashmap);
 }
 
-//Function to make requests
+// Function to make requests
 async fn make_req(url: String) -> Result<header::HeaderMap, Box<dyn std::error::Error>> {
     let client = Client::new();
     let res = client.get(url).timeout(Duration::from_secs(8)).send().await?.headers().clone();
