518293eee082f50c6eeda293be32c847157dbf9d
mech-keyboard69420 <keyboard69420@protonmail.com>
2022-10-28 03:00:23 +0300

    added comments to subdomains file

--- src/subdomains.rs
+++ src/subdomains.rs
@@ -1,23 +1,37 @@
 use std::process::Command;
 
 pub fn get_subs(scopes: Vec<String>) -> String {
+    // Declare empty subdomains vector
     let mut subdomains = Vec::new();
+
+    // Loop through all domain scopes
     for scope in scopes.iter() {
         println!("Getting subdomains for: {}", scope);
+
+        // Declare and run command to find subdomains
         let mut cmd = Command::new("findomain");
         cmd.args(["-q", "-t", scope]);
+
+        // Check if command ran seccusfully
         match cmd.output() {
             Ok(output) => {
-                let output_final = String::from_utf8(output.stdout).expect("Error formatting command output");
-                let mut output_final_clean = output_final.clone();
-                output_final_clean.pop();
-                output_final_clean.remove(0);
-                subdomains.push(output_final_clean);
-                println!("{}", output_final.clone());
+                // Get output
+                let mut output_final = String::from_utf8(output.stdout).expect("Error formatting command output");
+
+                // Remove clutter from output
+                output_final.remove(0);
+                output_final.pop();
+
+                // Print output
+                println!("\n{}\n", output_final.clone());
+
+                // Push output to subdomains vec
+                subdomains.push(output_final);
             },
             Err(error) => panic!("Error getting subdomain: {:?}", error),
         }
     }
-    let subdomains_joined = subdomains.join("");
-    subdomains_joined
+
+    // Return all subdomains joined
+    return subdomains.join("");
 }
