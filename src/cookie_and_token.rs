b3b0c765f07bbe788a85687b5ca6ffa49bf0190f
mech-keyboard69420 <keyboard69420@protonmail.com>
2022-10-28 03:09:43 +0300

    added comments to cookie_and_token file

--- src/cookie_and_token.rs
+++ src/cookie_and_token.rs
@@ -6,11 +6,22 @@ use std::collections::HashMap;
 #[tokio::main]
 
 pub async fn get_cookie_and_token() -> Result<HashMap<&'static str, String>, Box<dyn std::error::Error>> {
+    // Set url to get cookie and token from
     let url = "https://hackerone.com/reddit?type=team";
+    
+    // Create client
     let client = Client::new();
+
+    // Make a request to the url
     let res = client.get(url).send().await?;
+
+    // Get http headers of response
     let cookie_res = res.headers().clone();
+
+    // Get html of response
     let csrf_res = res.text().await?.clone();
+
+    // Parse csrf token out of html
     let csrf_doc = Html::parse_document(&csrf_res);
     let mut csrf = "";
     let selector = Selector::parse("meta[name=\"csrf-token\"]").unwrap();
@@ -21,12 +32,17 @@ pub async fn get_cookie_and_token() -> Result<HashMap<&'static str, String>, Box
 
     let mut headers_values: Vec<&str> = Vec::new();
 
+    // Loop through cookies
     for value in cookie_res.values() {
         headers_values.push(value.to_str().unwrap());
     }
+
     let host: Vec<&str> = headers_values[9].split(";").collect();
+
+    // Get the expire date
     let date = Local.datetime_from_str(host[2].replace(" expires=", "").as_str(), "%a, %d %h %Y %H:%M:%S GMT").unwrap();
 
+    // Get the cookie 
     let cookie = host[0];
 
     let mut hashmap: HashMap<&str, String> = HashMap::new();
@@ -34,5 +50,6 @@ pub async fn get_cookie_and_token() -> Result<HashMap<&'static str, String>, Box
     hashmap.insert("csrf", csrf.to_string());
     hashmap.insert("date", date.to_string());
 
-    Ok(hashmap)
+    // Return cookie, csrf token and expire date
+    return Ok(hashmap);
 }
